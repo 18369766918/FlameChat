e2c94299075d27bfdec58e54a715b67baf103fa4
Matthew <yanpeizhi2008@hotmail.com>
2017-02-03 15:04:54 -0500

    Login Alpha version 1
    can only add 1 user

--- FlameChat/Login.swift
+++ FlameChat/Login.swift
@@ -7,13 +7,44 @@
 //
 
 import UIKit
+import Firebase
 
 class Login: UIViewController {
 
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    var firebase: FIRDatabaseReference?
+    
+    /** Do any additional setup after loading the view. */
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.firebase = FIRDatabase.database().reference()
+        
+        if let user = FIRAuth.auth()?.currentUser{
+            
+            self.firebase!.child("users/\(user.uid)/userID").setValue(user.uid)
+            
+        }else{
+            
+            FIRAuth.auth()?.signInAnonymously(completion: { (user, error) in
+                if error != nil{
+                    
+                    let alert = UIAlertController(title: "Oops!", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alert.addAction(defaultAction)
+                    self.present(alert, animated: true, completion: nil)
+                    
+                    
+                }else{
+                    self.firebase!.child("users").child(user!.uid).setValue(["userID" : user!.uid])
+                }
+            })
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +53,41 @@ class Login: UIViewController {
     }
     
 
+    @IBAction func createAction(_ sender: AnyObject) {
+        
+        guard let email = self.emailField.text else {
+            return
+        }
+        guard let password = self.passwordField.text else{
+            return
+        }
+        
+        if email != "" && password != "" {
+            let credential = FIREmailPasswordAuthProvider.credential(withEmail: email, password: password)
+            
+            FIRAuth.auth()?.currentUser?.link(with: credential, completion: {(user,error) in
+                
+                if error != nil{
+                    
+                    let alert = UIAlertController(title: "Oops!", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alert.addAction(defaultAction)
+                    self.present(alert, animated: true, completion: nil)
+
+                }else{
+                    self.firebase!.child("users").child(user!.uid).setValue(["userID":user!.uid ])
+                    
+                }
+            })
+        }else{
+            
+            let alert = UIAlertController(title: "Oops!", message: "Please enter an email and password.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(defaultAction)
+            self.present(alert, animated: true, completion: nil)
+
+        }
+    }
     /*
     // MARK: - Navigation
 
