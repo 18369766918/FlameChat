94e9716f62eb8c27d7c223de6a83751afeec12af
Matthew <yanpeizhi2008@hotmail.com>
2017-02-03 22:11:47 -0500

    new feature!!! phone NO

--- FlameChat/Login.swift
+++ FlameChat/Login.swift
@@ -14,7 +14,8 @@ class Login: UIViewController {
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    
+    @IBOutlet weak var phoneField: UITextField!
+
     var firebase: FIRDatabaseReference?
     
     /** Do any additional setup after loading the view. */
@@ -23,6 +24,7 @@ class Login: UIViewController {
         
         /* database reference */
         self.firebase = FIRDatabase.database().reference()
+        
         
         if let user = FIRAuth.auth()?.currentUser{
             
@@ -46,6 +48,7 @@ class Login: UIViewController {
             
         }
         
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -62,6 +65,9 @@ class Login: UIViewController {
         guard let password = self.passwordField.text else{
             return
         }
+        guard let phone = self.phoneField.text else{
+            return
+        }
         
         if email != "" && password != "" {
             let credential = FIREmailPasswordAuthProvider.credential(withEmail: email, password: password)
@@ -76,7 +82,9 @@ class Login: UIViewController {
                     self.present(alert, animated: true, completion: nil)
 
                 }else{
-                    self.firebase!.child("users").child(user!.uid).setValue(["userID":user!.uid ,"passwd":password, "email": email, "online": false])
+                    self.firebase!.child("users").child(user!.uid).setValue(["passwd":password, "email": email])
+                    self.firebase!.child("id").setValue(phone)
+                    self.firebase!.child("id").child(phone).setValue(["passwd":password, "email": email, "online": false])
                     
                 }
             })
@@ -90,6 +98,7 @@ class Login: UIViewController {
         }
     }
     
+    
     /** log in button */
     @IBAction func login(_ sender: AnyObject) {
         FIRAuth.auth()?.signIn(withEmail: self.emailField.text!, password: self.passwordField.text!)
@@ -100,40 +109,19 @@ class Login: UIViewController {
                 print("\n\n\nLog in successfully! User: " + self.emailField.text! + "\n\n\n");
                 
                 var current = FIRAuth.auth()?.currentUser // current user reference
-                print("\n\n\n" + (current?.email)! + "\n\n\n");
-                print("\n\n\n" + (current?.uid)! + "\n\n\n");
+                //print("\n\n\n" + (current?.email)! + "\n\n\n");
+                //print("\n\n\n" + (current?.uid)! + "\n\n\n");
 
 
                /*
                 self.firebase!.child("users").queryOrdered(byChild: "email").queryEqual(toValue: "abc@abc.com").observeSingleEvent(of: .value, with: { snapshot in
                     
-                    
-                     var userID: String?
-                    
-                
-                    if let dictionary = snapshot.value as? [String: AnyObject]{
-                  
-                        
-                        
-                    }
-                    
-                       
-                        
-                        
-                        
-                        
-                        //self.navigationItem.title = dictionary["userID"] as? String
-                        //var UID = dictionary["userID"] as? String
-                        //print(UID as? String())
-                    }
-                    
-                    
-                    
+                    print(snapshot)
                     
                 })
-                
-                
-               */
+                */
+
+ 
                 
                 
                 /** change user status (online) to: true */
